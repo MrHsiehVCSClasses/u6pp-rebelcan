7168098de7ebe2f73db3a42edea7e3cf307b8aec
rebelcan <113130590+rebelcan@users.noreply.github.com>
2023-02-21 10:03:56 -0800

    Create Player.java

---
+++ src/main/java/u6pp/Player.java
@@ -0,0 +1,32 @@
+package u6pp;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private final String name;
+    private final List<Card> hand;
+
+    public Player(String name) {
+        this.name = name;
+        this.hand = new ArrayList<Card>();
+    }
+//setting up player name, hand and other basic functions like adding and removing cards
+    public String getName() {
+        return name;
+    }
+
+    public List<Card> getHand() {
+        return hand;
+    }
+
+    public void addCardToHand(Card card) {
+        hand.add(card);
+    }
+
+    public void removeCardFromHand(Card card) {
+        hand.remove(card);
+    }
+
+    public void setSaidUno(boolean b) {
+    }
+}
