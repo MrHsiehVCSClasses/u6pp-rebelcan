5f12c3212a85ef56fa4188bafedc9fe180621bf3
rebelcan <113130590+rebelcan@users.noreply.github.com>
2023-03-07 19:28:10 -0800

    Update Uno.java

--- src/main/java/u6pp/Uno.java
+++ src/main/java/u6pp/Uno.java
@@ -1,7 +1,4 @@
 package u6pp;
-
-
-
 import java.util.ArrayList;
 
 public class Uno {
@@ -18,6 +15,25 @@ public class Uno {
         this.discard = discard;
         this.currentPlayerIndex = startingPlayerIndex;
         this.isReversed = isReversed;
+        deck.shuffle();
+    }
+    public Uno(int playerCount) {
+        players = new ArrayList<Player>();
+        for(int i = 0; i < playerCount; i++){
+            System.out.println("ran");
+            players.add(new Player("p" + Integer.toString(i + 1)));
+        }
+        this.deck = CardStack.createUnoDeck();
+        this.discard = new CardStack();
+        this.discard.push(deck.pop());
+        this.currentPlayerIndex = 0;
+        this.isReversed = false;
+        deck.shuffle();
+        for(int i = 0; i < 7; i++){
+            for (Player curr : players){
+                curr.getHand().add(deck.pop());
+            }
+        }
     }
 //returns the current player in the game.
     public Player getCurrentPlayer() {
@@ -45,10 +61,20 @@ public class Uno {
     //checks if the current player has the card in their hand, and if it can be played on top of the discard pile
     public boolean playCard(Card card, String color) {
         Player currentPlayer = getCurrentPlayer();
+        if(deck.getSize() - 1 <= 0){
+            System.out.print("ran");
+            Card tempCard = discard.pop();
+            deck.addAll(discard);
+            discard.push(tempCard);
+        }
+        if (color == null || card == null){
+            currentPlayer.addCardToHand(deck.pop());
+            getNextPlayer();
+        }
         if (!currentPlayer.getHand().contains(card)) {
             return false;
         }
-        if (!card.canPlayOn(getTopDiscard())) {
+        else if (!card.canPlayOn(getTopDiscard())) {
             return false;
         }
         //If both conditions are met, the card is removed from the player's hand and added to the discard pile
@@ -58,17 +84,36 @@ public class Uno {
         if (currentPlayer.getHand().size() == 1) {
             currentPlayer.setSaidUno(true);
         }
-        if (currentPlayer.getHand().size() == 0) {
+        else if (card.getValue() == Card.SKIP && card.canPlayOn(getTopDiscard())) {
+            getNextPlayer();
+        }
+        else if (card.getValue() == Card.DRAW_2 && card.canPlayOn(getTopDiscard())) {
+            getNextPlayer();
+            players.get(currentPlayerIndex).addCardToHand(deck.pop());
+            players.get(currentPlayerIndex).addCardToHand(deck.pop());
+        }
+        else if (card.getValue() == Card.WILD && !(card.getColor() == Card.WILD_DRAW_4)) {
+            getTopDiscard().trySetColor(color);
+        }
+        else if (card.getValue() == Card.WILD_DRAW_4 || card.getColor() == Card.WILD_DRAW_4) {
+            getTopDiscard().trySetColor(color);
+            getNextPlayer();
+            players.get(currentPlayerIndex).addCardToHand(deck.pop());
+            players.get(currentPlayerIndex).addCardToHand(deck.pop());
+            players.get(currentPlayerIndex).addCardToHand(deck.pop());
+            players.get(currentPlayerIndex).addCardToHand(deck.pop());
+        }
+        else if (currentPlayer.getHand().size() == 0) {
             return true;
         }
-        if (card.getRank() == Card.REVERSE) {
+        else if (card.getValue() == Card.REVERSE) {
             isReversed = !isReversed;
         }
         getNextPlayer();
         return true;
     }
 
-//calculates the index of the next player
+//calculates the index of the next player.
     private int getNextPlayerIndex() {
         int direction = isReversed ? -1 : 1;
         int nextPlayerIndex = (currentPlayerIndex + direction + players.size()) % players.size();
@@ -76,6 +121,6 @@ public class Uno {
     }
 
     public ArrayList<Player> getPlayers() {
-        return null;
+        return players;
     }
 }
